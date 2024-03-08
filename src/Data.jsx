01efa0f4e6fa622e156d737c33cd0fe94dd8ac58
Gau <gauravgimonkar@gmail.com>
2024-03-09 02:09:01 +0530

    first commit loading all files first time

---
+++ src/Data.jsx
@@ -0,0 +1,283 @@
+const Menu=[
+    {
+        id:1,
+        image:"images/veg-biryani.jpg",
+        name:"veg biryani",
+        category:"dinner",
+        price:"220rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi..",
+    },
+    {
+        id:2,
+        image:"images/samosa.jpg",
+        name:"samosa",
+        category:"breakfast",
+        price:"30rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },
+
+    {
+        id:3,
+        image:"images/gulab-jamun.jpg",
+        name:"gulab jamun",
+        category:"sweet",
+        price:"40rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.", 
+    }, 
+    
+    {
+        id:4,
+        image:"/images/panir-achari.jpg",
+        name:"paneer achari",
+        category:"lunch",
+        price:"259rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:5,
+        image:"images/chocolet-shake.jpg",
+        name:"chocolet shake",
+        category:"sweet",
+        price:"120rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, {
+        id:6,
+        image:"images/kachori.jpg",
+        name:"kachori",
+        category:"breakfast",
+        price:"30rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:7,
+        image:"images/rajma-chawal.jpg",
+        name:"rajma chawal",
+        category:"dinner",
+        price:"90rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },
+    {
+        id:8,
+        image:"images/maggi.jpg",
+        name:"Maggi",
+        category:"breakfast",
+        price:"30rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:9,
+        image:"images/aalu-gobi.jpg",
+        name:"aalu gobi",
+        category:"lunch",
+        price:"120rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:11,
+        image:"images/burgur.jpg",
+        name:"Burgur",
+        category:"breakfast",
+        price:"149rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:12,
+        image:"/images/kheer.jpg",
+        name:"Kheer",
+        category:"sweet",
+        price:"160rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:13,
+        image:"images/chana-masala.jpg",
+        name:"chana-masala",
+        category:"dinner",
+        price:"180rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },  {
+        id:14,
+        image:"images/paratha.jpg",
+        name:"paratha",
+        category:"lunch",
+        price:"70rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    }, 
+    {
+        id:15,
+        image:"images/poha.jpg",
+        name:"poha",
+        category:"breakfast",
+        price:"40rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },
+    {
+        id:16,
+        image:"images/maggi.jpg",
+        name:"Maggi",
+        category:"breakfast",
+        price:"30rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:17,
+        image:"images/jalebi.jpg",
+        name:"jalebi",
+        category:"sweet",
+        price:"120rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:17,
+        image:"images/dosa.jpg",
+        name:"Dosa",
+        category:"breakfast",
+        price:"149rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:19,
+        image:"/images/sabudana-khichadi.jpg",
+        name:"sabudana khichadi",
+        category:"breakfast",
+        price:"120rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi. ",
+    }, 
+    {
+        id:20,
+        image:"images/dhokla.jpg",
+        name:"Dhokla",
+        category:"breakfast",
+        price:"130rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },  {
+        id:21,
+        image:"images/sandwich.jpg",
+        name:"sandwich",
+        category:"evening",
+        price:"90rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    }, 
+    {
+        id:22,
+        image:"images/puneer-butter-masala.jpg",
+        name:"paneer butter masala",
+        category:"dinner",
+        price:"90rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:23,
+        image:"images/sandwich.jpg",
+        name:"sandwich",
+        category:"breakfast",
+        price:"80rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:24,
+        image:"images/peda.jpg",
+        name:"peda",
+        category:"sweet",
+        price:"100rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:25,
+        image:"images/kalakand.jpg",
+        name:"kalakand",
+        category:"sweet",
+        price:"90rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:26,
+        image:"images/mysore-pak.jpg",
+        name:"mysore pak",
+        category:"sweet",
+        price:"90rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:27,
+        image:"images/chana-masala.jpg",
+        name:"chana masala",
+        category:"dinner",
+        price:"90rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:28,
+        image:"images/kadhi-chawal.jpg",
+        name:"kadhi chawal",
+        category:"dinner",
+        price:"120rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:29,
+        image:"images/dal-makhani.jpg",
+        name:"paneer butter masala",
+        category:"dinner",
+        price:"230rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:30,
+        image:"images/palak-paneer.jpg",
+        name:"palak paneer",
+        category:"dinner",
+        price:"239rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:31,
+        image:"images/veg-pulav.jpg",
+        name:"veg pulav",
+        category:"dinner",
+        price:"110rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:32,
+        image:"images/bhindi-masala.jpg",
+        name:"bhindi masala",
+        category:"lunch",
+        price:"220rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:33,
+        image:"images/gujrati-kadhi.jpg",
+        name:"gujrati kadhi",
+        category:"lunch",
+        price:"180rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:34,
+        image:"images/egg-curry.jpg",
+        name:"egg curry",
+        category:"lunch",
+        price:"150rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },    {
+        id:35,
+        image:"images/chiken-curry.jpg",
+        name:"chiken curry",
+        category:"dinner",
+        price:"350rs",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptates quasi.",
+        
+    },   
+]
+
+export default Menu
---
+++ src/home.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import Menu from "./Data";
+
+const Home = () => {
+    const [Data,SetData] = useState(Menu)
+
+    const FilterItem=(categ)=>{
+        const updatedItem=Menu.filter((item)=>{
+            return item.category===categ
+        })
+        SetData(updatedItem)
+    }
+    return (
+        <>
+        {/* Order Your Favourite Dish */}
+            <h1 className="main_heading">gaurav's personel food court</h1>
+            <hr />
+            <div className="buttons_div">
+                <button type="button" class="btn but btn-danger"onClick={()=>SetData(Menu)}>All Items</button>
+                <button type="button" class="btn btn-danger" onClick={()=>FilterItem('breakfast')}>Breakfast</button>
+                <button type="button" class="btn btn-danger" onClick={()=>FilterItem('dinner')}>Dinner</button>
+                <button type="button" class="btn btn-danger" onClick={()=>FilterItem('lunch')}>Lunch</button>
+                <button type="button" class="btn btn-danger" onClick={()=>FilterItem('sweet')}>Sweet</button>
+            </div>
+            <hr />
+
+            
+
+
+           <div className="cardsMain">
+
+            {
+                Data.map((item)=>{
+                    // const {image,name,category,price,description} = Data
+                    return(
+                        <div className="card">
+                        <div className="cardImg">
+                            <img src={item.image} className="item_image" alt="maggi" />
+                        </div>
+                        <div className="cardBody">
+                            <h1>{item.name}</h1>
+                            <p>{item.description}</p>
+                            <div className="innergreed">
+                                <h3>price :{item.price}</h3>
+                                <a href="/">
+                                <button type="button" class="btn btn-danger">Order Now</button>
+                                </a>
+                            </div>
+                            <p>Prices may Vary on Selected Date</p>
+    
+                        </div>
+                    </div>
+                    )
+                })
+            }
+
+
+
+
+           </div>
+
+
+        </>
+    )
+}
+
+export default Home
